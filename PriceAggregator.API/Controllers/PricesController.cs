97395ff0f7adeed4617be47f889937a2c7a6786f
Lidia Necula <>
2023-06-02 21:21:36 +0400

    Initial setup

---
+++ PriceAggregator.API/Controllers/PricesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PriceAggregator.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PricesController : ControllerBase
+{
+    private readonly ILogger<PricesController> _logger;
+
+    public PricesController(ILogger<PricesController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<PriceModel> Get()
+    {
+        return new List<PriceModel>
+        {
+            new()
+            {
+                Price = 10,
+                Time = DateTime.Now
+            }
+        };
+    }
+}
+
---
+++ PriceAggregator.API/PriceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PriceAggregator.API;
+
+public class PriceModel
+{
+    public DateTime Time { get; set; }
+    
+    public double Price { get; set; }
+}
+
