2e99b003f950c0f5c42acc4f2c72258aa7cabf16
Lidia Necula <>
2023-06-05 20:18:01 +0400

    Add more tests

--- PriceAggregator.IntegrationTests/PriceControllerTests.cs
+++ PriceAggregator.IntegrationTests/PriceControllerTests.cs
@@ -18,7 +18,7 @@
         }
         
         [Fact]
-        public async Task GivenTheRequestIsValid_WhenGettingTheAggregatedPrice_TheItShouldReturnThePrice()
+        public async Task GivenTheRequestIsValid_WhenGettingTheAggregatedPrice_ThenItShouldReturnThePrice()
         {
             // Arrange
             var client = _factory.CreateClient();
@@ -36,7 +36,25 @@
         }
         
         [Fact]
-        public async Task GetPersistedPrices_ValidRequest_ReturnsPricesInRange()
+        public async Task GivenTheExternalServicesAreReturningBadRequest_WhenGettingTheAggregatedPrice_ThenItShouldReturnTheDefaultPrice()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            var time = new DateTime(2023, 2, 1, 12, 14, 15);
+   
+            // Act
+            var response = await client.GetAsync($"/api/v1/prices/btcusd/{time:yyyy-MM-ddTHH:mm:ss}");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var price = JsonConvert.DeserializeObject<double>(await response.Content.ReadAsStringAsync());
+            price.Should().Be(0);
+        }
+        
+        [Fact]
+        public async Task GivenValidRequest_WhenGettingThePersistedPrices_ThenItShouldReturnPricesInRange()
         {
             // Arrange
             var client = _factory.CreateClient();
@@ -56,6 +74,53 @@
 
             var prices = JsonConvert.DeserializeObject<AggregatedPriceModel[]>(await response.Content.ReadAsStringAsync());
             prices.Should().NotBeNullOrEmpty();
-            prices!.Length.Should().BeGreaterThan(0);
+            prices!.Length.Should().Be(4);
+        }
+        
+        [Fact]
+        public async Task GivenTheRequestedInstrumentDoesNotExist_WhenGettingThePersistedPrices_ThenItShouldReturnPricesInRange()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            var startTime = new DateTime(2023, 6, 1, 6, 0, 0); 
+            var endTime = new DateTime(2023, 6, 1, 13, 59, 59);
+
+            using var scope = _factory.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<FinancialInstrumentsDbContext>();
+            TestDataSeeder.PopulateDatabase(dbContext);
+
+            // Act
+            var response = await client.GetAsync($"/api/v1/prices/persisted/random?startTime={startTime:yyyy-MM-ddTHH:mm:ss}&endTime={endTime:yyyy-MM-ddTHH:mm:ss}");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var prices = JsonConvert.DeserializeObject<AggregatedPriceModel[]>(await response.Content.ReadAsStringAsync());
+            prices!.Should().BeEmpty();
+        }
+        
+        [Fact]
+        public async Task GivenThereIsNoSavedPriceForTheRange_WhenGettingThePersistedPrices_ThenItShouldReturnPricesInRange()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            var startTime = new DateTime(2023, 6, 1, 6, 0, 0); 
+            var endTime = new DateTime(2023, 6, 1, 13, 59, 59);
+
+            using var scope = _factory.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<FinancialInstrumentsDbContext>();
+            TestDataSeeder.PopulateDatabase(dbContext);
+
+            // Act
+            var response = await client.GetAsync($"/api/v1/prices/persisted/btcusd?startTime={startTime:yyyy-MM-ddTHH:mm:ss}&endTime={endTime:yyyy-MM-ddTHH:mm:ss}");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var prices = JsonConvert.DeserializeObject<AggregatedPriceModel[]>(await response.Content.ReadAsStringAsync());
+            prices.Should().NotBeNullOrEmpty();
+            prices!.Length.Should().Be(2);
         }
     }
